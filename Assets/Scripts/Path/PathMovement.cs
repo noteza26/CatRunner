df8bf34e12c91c79f3fd27bc45162ccd9384995a
BuildToolsUnity <imilly556553@hotmail.com>
2021-03-13 00:13:38 +0700

    Edit camera

--- Assets/Scripts/Path/PathMovement.cs
+++ Assets/Scripts/Path/PathMovement.cs
@@ -6,12 +6,13 @@ public class PathMovement : MonoBehaviour
 {
     internal GameObject SetDeletePoint { set { deletePoint = value; } }
     [SerializeField] GameObject deletePoint;
+    [SerializeField] bool dontDestroy;
 
     void Update()
     {
         if (PlayerManager.instance.IsStop || PlayerManager.instance.GodStop) return;
         this.transform.position -= Vector3.forward * PathManager.instance.nowSpeed * Time.deltaTime;
-        if (deletePoint == null)
+        if (deletePoint == null && !dontDestroy)
             Destroy(this.gameObject);
     }
 }
